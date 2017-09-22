6cb7b2de71102aced64357ea9e81d9c55d8827b6
ittigorn <ittigorn@bitbucket.org>
2017-09-22 15:23:50 +0700

    no message

--- src/app/surveys/died/died.component.ts
+++ src/app/surveys/died/died.component.ts
@@ -8,6 +8,10 @@ declare var $: any;
 export class DiedComponent implements OnInit {
   show = false;
   location = 1;
+  cause = 1;
+  location2 = 1;
+  cause2 = 1;
+
   constructor() { }
 
   ngOnInit() {
@@ -16,16 +20,43 @@ export class DiedComponent implements OnInit {
       startDate: '-3d'
   });
 
-  $("#textarea").hide();
+  $("#textLocation").hide();
+  $("#textCause").hide();
+
+  $("#textLocation2").hide();
+  $("#textCause2").hide();
+  
   }
 
-  onChange(){
-    console.log(this.location);
-
+  ChangeLocation(){
     if(this.location == 0){
-      $("#textarea").fadeIn();
+      $("#textLocation").fadeIn();
     }else{
-      $("#textarea").fadeOut();
+      $("#textLocation").fadeOut();
+    }
+  }
+
+  ChangeCause(){
+    if(this.cause == 0){
+      $("#textCause").fadeIn();
+    }else{
+      $("#textCause").fadeOut();
+    }
+  }
+
+  ChangeLocation2(){
+    if(this.location2 == 0){
+      $("#textLocation2").fadeIn();
+    }else{
+      $("#textLocation2").fadeOut();
+    }
+  }
+
+  ChangeCause2(){
+    if(this.cause2 == 0){
+      $("#textCause2").fadeIn();
+    }else{
+      $("#textCause2").fadeOut();
     }
   }
 
