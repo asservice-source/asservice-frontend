d9c46b1e5b4444552cc9d46bd6b740e9f4417315
ittigorn <ittigorn@bitbucket.org>
2017-09-22 14:07:34 +0700

    no message

--- src/app/surveys/died/died.component.ts
+++ src/app/surveys/died/died.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-died',
   templateUrl: './died.component.html',
   styleUrls: ['./died.component.css']
 })
 export class DiedComponent implements OnInit {
-
+  show = false;
+  location = 1;
   constructor() { }
 
   ngOnInit() {
+    $('.datepicker').datepicker({
+      format: 'mm/dd/yyyy',
+      startDate: '-3d'
+  });
+
+  $("#textarea").hide();
   }
+
+  onChange(){
+    console.log(this.location);
+
+    if(this.location == 0){
+      $("#textarea").fadeIn();
+    }else{
+      $("#textarea").fadeOut();
+    }
+  }
+
 
 }
