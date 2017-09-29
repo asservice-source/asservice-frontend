7e53334833d1a941bcfc715c6b1cf3517d56132a
FREEWILLGROUP\Chanachai_Ote <chanachai.ot@rmuti.ac.th>
2017-09-29 16:49:28 +0700

    no message

--- src/app/surveys/mosquito/mosquito.component.ts
+++ src/app/surveys/mosquito/mosquito.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mosquito.component.css']
 })
 export class MosquitoComponent implements OnInit {
-
+  mLocationNo = 0;
+  
+   isDisable= true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  changLocationNo(){
+    if(this.mLocationNo==1){
+      this.isDisable = false;
+   
+    }else{
+      this.isDisable = true;
+ 
+    }
+    
+  }
 }
