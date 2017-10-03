3634fa530ca212fef8105512476687813e124270
FREEWILLGROUP\Chanachai_Ote <chanachai.ot@rmuti.ac.th>
2017-10-03 14:49:40 +0700

    no message

--- src/app/surveys/cancer/cancer.component.ts
+++ src/app/surveys/cancer/cancer.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CancerComponent implements OnInit {
 
+  mStatusNo = 0;
+  
+  isDisable= true;
   constructor() { }
 
   ngOnInit() {
+  }
+  changStatusNo(){
+    if(this.mStatusNo==21){
+      this.isDisable = false ;
+    
+    }else{
+      this.isDisable = true ;
+    }
   }
 
 }
