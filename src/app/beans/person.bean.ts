8eb24f1727d189a0fe35b118deef0a8e07652a89
FREEWILLGROUP\Terdsak_Pot <terdsak_pot>
2017-10-04 11:26:16 +0700

    no message

---
+++ src/app/beans/person.bean.ts
@@ -0,0 +1,7 @@
+import {  } from "@angular/core";
+export class PersonBean{
+    public citizenID: number;
+    public lastName: string;
+    public firstName: string;
+    public nickName: string;
+}
