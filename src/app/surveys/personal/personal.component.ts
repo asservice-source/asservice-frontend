4e48022091a6426237700988a7a75fb23811d87c
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2017-10-05 14:17:43 +0700

    Edit personal.

--- src/app/surveys/personal/personal.component.ts
+++ src/app/surveys/personal/personal.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Http } from "@angular/http";
 import { Router } from "@angular/router";
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs/Rx';
 import { FilterBean } from "../../beans/filter.bean";
+declare var $;
 
 @Component({
   selector: 'app-personal',
@@ -9,7 +12,12 @@ import { FilterBean } from "../../beans/filter.bean";
   styleUrls: ['./personal.component.css']
 })
 
-export class PersonalComponent implements OnInit {
+export class PersonalComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+
+  dtTrigger: Subject<any> = new Subject();
 
   // Datatables options
   dtOptions: DataTables.Settings = {};
@@ -21,14 +29,22 @@ export class PersonalComponent implements OnInit {
   cid: string;
 
   constructor(private http: Http, private router: Router) {
-    
+
   }
 
   ngOnInit(): void {
 
   }
 
-  loadData() {
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
+  }
+
+  clickSearch(event: FilterBean) {
+
+    let osmId = event.OSMID;
+    let homeId = event.homeID;
+    let villageId = event.villageID;
 
     this.http.get("assets/data_test/data_home_personal.json")
       .map(res => res.json())
@@ -40,11 +56,11 @@ export class PersonalComponent implements OnInit {
       columns: [{
         width: "15%"
       }, {
+        width: "15%"
+      }, {
+        width: "15%"
+      }, {
         width: "40%"
-      }, {
-        width: "15%"
-      }, {
-        width: "15%"
       }, {
         width: "10%"
       }, {
@@ -53,14 +69,14 @@ export class PersonalComponent implements OnInit {
       }]
     };
 
-  }
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      // Destroy the table first
+      dtInstance.destroy();
+      // Call the dtTrigger to rerender again
+      this.dtTrigger.next();
+    });
 
-  clickSearch(event: FilterBean) {
-    let osmId = event.OSMID;
-    let homeId = event.homeID;
-    let villageId = event.villageID;
-
-    this.loadData();
+    // $("#rowTable").show();
   }
 
   clickManage(key: string) {
