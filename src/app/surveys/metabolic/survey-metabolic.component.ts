a6b0d7172ac4fd788cfd5d5a79ec2026ffc7f3c7
ittigorn <ittigorn@bitbucket.org>
2017-10-12 17:41:49 +0700

    no message

--- src/app/surveys/metabolic/survey-metabolic.component.ts
+++ src/app/surveys/metabolic/survey-metabolic.component.ts
@@ -3,6 +3,9 @@ import { Http, Response, RequestOptions } from "@angular/http";
 import { Router } from "@angular/router";
 import { BaseComponent } from "./../../base-component";
 import { PersonBean } from "../../beans/person.bean";
+import { ApiHTTPService } from '../../service/api-http.service';
+import { ActionCustomViewComponent } from '../../action-custom-table/action-custom-view.component';
+import { HeadFilterBean } from '../../beans/survey-head-filter.Bean';
 
 declare var $;
 
@@ -15,21 +18,88 @@ export class SurveyMetabolicComponent extends BaseComponent implements OnInit {
 
   public year = '2560';
   public citizenID: string = "0";
-  public dtOptions: DataTables.Settings = {};
+
   public data;
   public mooID: number = 0;
   public xxx: string;
   public check: boolean = false;
   public metabolicHeadID: number = 0;
   public surveyTypeCode: string = "METABOLIC"; 
+  public sources: any;
+  public isHideList: boolean = true;
+  
+
+  private api: ApiHTTPService;
+  public settings: any;
 
   constructor(private http: Http, private router: Router) {
     super();
     this.loadData();
+    this.api = new ApiHTTPService();
+    let self = this;
+    this.settings = this.getTabelSetting({
+      id: {
+        title: 'ลำดับ',
+        filter: false,
+        sort: false,
+        width: '60px',
+      },
+      name: {
+        title: 'ชื่อ - นามสกุล',
+        filter: false,
+      },
+      cID: {
+        title: 'เลขประจำตัวประชาชน',
+        filter: false, 
+        width: '200px',
+      },
+      homeID: {
+        title: 'บ้านเลขที่',
+        filter: false, 
+        width: '180px',
+      },
+      gender: {
+        title: 'เพศ',
+        filter: false ,
+        width: '70px',
+      },
+      age: {
+        title: 'อายุ',
+        filter: false,
+        width: '90px',
+      },
+      action: {
+        title: 'การทำงาน',
+        filter: false,
+        sort: false,
+        width: '100px',
+        type: 'custom',
+        renderComponent: ActionCustomViewComponent,
+        onComponentInitFunction(instance) {
+          // instance.view.subscribe(row => {
+          //   self.doClick(row);
+          // });
+          // instance.edit.subscribe(row => {
+          //   self.doClick(row);
+          // });
+          // instance.delete.subscribe(row => {
+          //   self.doClick(row);
+          // });
+
+          instance.action.subscribe(row => {
+            self.doClick(row);
+          });
+        }
+      }
+    });
+  }
+
+  doClick(row){
+    alert(row.id);
   }
 
   ngOnInit() {
-
+    
   }
 
   save() {
@@ -41,37 +111,47 @@ export class SurveyMetabolicComponent extends BaseComponent implements OnInit {
       .map(res => res.json())
       .subscribe(data => this.data = data);
 
-    this.dtOptions = {
-      pagingType: "full_numbers",
-      processing: true,
-      columns: [{
-        width: "40px",
-        orderable: false
-      }, {
-        width: ""
-      }, {
-        width: "220px"
-      }, {
-        width: "70px"
-      }, {
-        width: "70px"
-      }, {
-        width: "70px"
-      }, {
-        width: "70px"
-      }, {
-        width: "70px"
-      }, {
-        width: "70px",
-        orderable: false
-      }]
-    };
-
+    // this.dtOptions = {
+    //   pagingType: "full_numbers",
+    //   processing: true,
+    //   columns: [{
+    //     width: "40px",
+    //     orderable: false
+    //   }, {
+    //     width: ""
+    //   }, {
+    //     width: "220px"
+    //   }, {
+    //     width: "70px"
+    //   }, {
+    //     width: "70px"
+    //   }, {
+    //     width: "70px"
+    //   }, {
+    //     width: "70px"
+    //   }, {
+    //     width: "70px"
+    //   }, {
+    //     width: "70px",
+    //     orderable: false
+    //   }]
+    // };
   }
 
   openModal(key: string) {
     this.citizenID = key;
     $("#addMetabolicSurvey").modal('show');
+
+  }
+
+  onChangeFilter(event: HeadFilterBean){
+    console.log("ChangeFilter");
+    this.isHideList = true;
+  }
+  onSearch(event: HeadFilterBean){
+      console.log(event);
+      this.sources = this.data;
+      this.isHideList = false;
 
   }
 
--- src/app/surveys/patient/survey-patient.component.ts
+++ src/app/surveys/patient/survey-patient.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../../base-component';
+import { ApiHTTPService } from '../../service/api-http.service';
+import { ActionCustomViewComponent } from '../../action-custom-table/action-custom-view.component';
+import { HeadFilterBean } from '../../beans/survey-head-filter.Bean';
 declare var $;
 @Component({
   selector: 'app-survey-patient',
   templateUrl: './survey-patient.component.html',
   styleUrls: ['./survey-patient.component.css']
 })
-export class SurveyPatientComponent implements OnInit {
+export class SurveyPatientComponent extends BaseComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {};
 
@@ -13,8 +17,130 @@ export class SurveyPatientComponent implements OnInit {
   public isShowsick : boolean = true;
   public surveyTypeCode: string = "PATIENT";
 
+  private api: ApiHTTPService;
+  public settings: any;
+  public isHideList: boolean = true;
+  public sources: any;
+  public datas = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      citizenId: "1-4113-00-1349-8-9",
+      reason: "อุบัติเหตุทางรถยนต์",
+      gender: "ชาย",
+      age: 38,
+      type: "ติดบ้าน"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      citizenId: "1-4113-00-1349-8-0",
+      reason: "อุบัติเหตุทางรถยนต์",
+      gender: "ชาย",
+      age: 54,
+      type: "ติดบ้าน"
+      
+    },
+    {
+      id: 11,
+      name: "Nicholas DuBuque",
+      citizenId: "1-4113-00-2259-6-4",
+      reason: "อุบัติเหตุทางรถยนต์",
+      gender: "ชาย",
+      age: 32,
+      type: "ติดบ้าน"
+     
+    },
+    {
+      id: 12,
+      name: "Nicholas DuBuque",
+      citizenId: "1-4113-00-2254-6-2",
+      reason: "อุบัติเหตุทางรถยนต์",
+      gender: "ชาย",
+      age: 62,
+      type: "ติดบ้าน"
+     
+    },
+    {
+      id: 13,
+      name: "Nicholas DuBuque",
+      citizenId: "1-4113-00-3259-6-5",
+      reason: "อุบัติเหตุทางรถยนต์",
+      gender: "ชาย",
+      age: 42,
+      type: "ติดบ้าน"
+     
+    },
+  ];
+
   constructor() { 
-    this.loadData();
+    super();
+    this.api = new ApiHTTPService();
+    let self = this;
+    this.settings = this.getTabelSetting({
+      id: {
+        title: 'ลำดับ',
+        filter: false,
+        sort: false,
+        width: '60px',
+      },
+      name: {
+        title: 'ชื่อ - นามสกุล',
+        filter: false
+      },
+      citizenId: {
+        title: 'เลขประจำตัวประชาชน',
+        filter: false, 
+        width: '200px',
+      },
+      reason: {
+        title: 'สาเหตุความพิการ/ป่วย',
+        filter: false, 
+        width: '180px',
+      },
+      gender: {
+        title: 'เพศ',
+        filter: false ,
+        width: '70px',
+      },
+      age: {
+        title: 'อายุ',
+        filter: false,
+        width: '90px',
+      },
+      type: {
+        title: 'ประเภทผู้พิการ/ป่วย',
+        filter: false,
+        width: '90px',
+      },
+      action: {
+        title: 'การทำงาน',
+        filter: false,
+        sort: false,
+        width: '100px',
+        type: 'custom',
+        renderComponent: ActionCustomViewComponent,
+        onComponentInitFunction(instance) {
+          // instance.view.subscribe(row => {
+          //   self.doClick(row);
+          // });
+          // instance.edit.subscribe(row => {
+          //   self.doClick(row);
+          // });
+          // instance.delete.subscribe(row => {
+          //   self.doClick(row);
+          // });
+
+          instance.action.subscribe(row => {
+            self.doClick(row);
+          });
+        }
+      }
+    });
+  }
+
+  doClick(row){
+    alert(row.id);
   }
 
   ngOnInit() {  
@@ -31,27 +157,38 @@ export class SurveyPatientComponent implements OnInit {
 
   loadData() {
   
-      this.dtOptions = {
-        pagingType: "full_numbers",
-        processing: true,
-        columns: [{
-          width: "40px",
-          orderable: false
-        }, {
-          width: ""
-        }, {
-          width: "200px"
-        }, {
-          width: "50px"
-        }, {
-          width: "170px"
-        }, {
-          width: "100px"
-        }, {
-          width: "70px",
-          orderable: false
-        }]
-      };
+      // this.dtOptions = {
+      //   pagingType: "full_numbers",
+      //   processing: true,
+      //   columns: [{
+      //     width: "40px",
+      //     orderable: false
+      //   }, {
+      //     width: ""
+      //   }, {
+      //     width: "200px"
+      //   }, {
+      //     width: "50px"
+      //   }, {
+      //     width: "170px"
+      //   }, {
+      //     width: "100px"
+      //   }, {
+      //     width: "70px",
+      //     orderable: false
+      //   }]
+      // };
+  
+    }
+
+    onChangeFilter(event: HeadFilterBean){
+      console.log("ChangeFilter");
+      this.isHideList = true;
+    }
+    onSearch(event: HeadFilterBean){
+        console.log(event);
+        this.sources = this.datas;
+        this.isHideList = false;
   
     }
 
