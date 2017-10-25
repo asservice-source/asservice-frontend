2b7808532b138f3853dc8f8aa827e30bbd27474d
FREEWILLGROUP\Terdsak_Pot <terdsak_pot>
2017-10-25 13:42:31 +0700

    no message

--- src/app/main/main-left-side/main-left-side.component.ts
+++ src/app/main/main-left-side/main-left-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { ApiHTTPService } from "../../service/api-http.service";
 
 @Component({
   selector: 'app-main-left-side',
@@ -6,10 +7,21 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./main-left-side.component.css']
 })
 export class MainLeftSideComponent implements OnInit {
-  @Input() links: Array<any> = [];
+  //@Input() links: Array<any> = [];
+  public links: Array<any> = [];
+  private api: ApiHTTPService = new ApiHTTPService();
   constructor() { }
 
   ngOnInit() {
+    this.setUpMenu();
+  }
+
+  setUpMenu(){
+    let self = this;
+    this.api.post('app/menu',{}, function(response){
+        console.log(response);
+        self.links = response;
+    });
   }
 
 }
