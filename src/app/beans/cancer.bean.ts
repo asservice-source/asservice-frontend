fb98631b29f9ef2ebc756306f0956370c34b4b7d
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2017-10-30 09:38:13 +0700

    no message

---
+++ src/app/beans/cancer.bean.ts
@@ -0,0 +1,11 @@
+export class CancerBean{
+    public fullName: string;
+    public age: number;
+    public cancerType: string;
+    public hospital: string;
+    public sickDate: string;
+    public tel: string;
+    public other: string;
+    public status: string;
+    public inputDate: string;
+}
