18ea1fa0f3df11a92c718012cb3e7e22db398b0f
FREEWILLGROUP\Sittichok_Kum <sittichok_kum@freewillsolutions.com>
2017-11-14 17:38:28 +0700

    no message

--- src/app/surveys/cancer/survey-cancer-form/survey-cancer-form.component.ts
+++ src/app/surveys/cancer/survey-cancer-form/survey-cancer-form.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CancerBean } from '../../../beans/cancer.bean';
+import { IMyDpOptions, IMyDateModel } from 'mydatepicker';
+import { PersonalMemberBean } from '../../../beans/personal-member.bean';
+import { BaseComponent } from '../../../base-component';
 
 @Component({
   selector: 'app-survey-cancer-form',
   templateUrl: './survey-cancer-form.component.html',
   styleUrls: ['./survey-cancer-form.component.css']
 })
-export class SurveyCancerFormComponent implements OnInit {
+export class SurveyCancerFormComponent extends BaseComponent implements OnInit {
+
+  public member: PersonalMemberBean = new PersonalMemberBean();
 
   @Input() set triggerCancer(paramCancer: CancerBean) {
     // this.showModal(paramsPerson);
   }
 
   constructor() {
-
+    super();
   }
 
   ngOnInit() {
@@ -24,4 +29,10 @@ export class SurveyCancerFormComponent implements OnInit {
     let self = this;
   }
 
+  onChangeDate(event: IMyDateModel) {
+    let self = this;
+
+    // console.log(event);
+    self.member.birthDate = self.getStringDateForDatePickerModel(event.date);
+  }
 }
