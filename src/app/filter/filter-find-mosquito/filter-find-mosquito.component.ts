0e214f27b9a2ebb904cc09d4b0b17907e9b3ec6c
ittigorn <ittigorn@bitbucket.org>
2017-11-23 12:09:29 +0700

    no message

--- src/app/filter/filter-find-mosquito/filter-find-mosquito.component.ts
+++ src/app/filter/filter-find-mosquito/filter-find-mosquito.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BaseComponent } from '../../base-component';
 import { ApiHTTPService } from '../../service/api-http.service';
+import {findHomeBean} from '../../beans/findhome.bean';
 
 @Component({
   selector: 'app-filter-find-mosquito',
@@ -9,11 +10,17 @@ import { ApiHTTPService } from '../../service/api-http.service';
 })
 export class FilterFindMosquitoComponent extends BaseComponent implements OnInit {
 
+  @Input() findHome: boolean;
+  @Input() reset: any;
+
 
   public villageData : any;
   private api: ApiHTTPService;
   public isShowFind = true;
   public HomeTypeData : any;
+  public HomeNameData : any;
+  public isHomeDisable : boolean = true;
+  public findhomebean: findHomeBean = new findHomeBean();
 
   constructor() {
     super();
@@ -24,6 +31,20 @@ export class FilterFindMosquitoComponent extends BaseComponent implements OnInit
 
   ngOnInit() {
     
+  }
+
+  ngOnChanges(changes): void {
+    console.log("OnChanges");
+    console.log(changes);
+    if (changes['findHome']) {
+      this.isShowFind = this.findHome;
+      
+    }
+    // if(changes['reset']){
+    //   this.filterBean.villageId = "";
+    //   this.changVillageNo();
+      
+    // }
   }
 
   setupVillage() {
@@ -41,8 +62,32 @@ export class FilterFindMosquitoComponent extends BaseComponent implements OnInit
         console.log(resp.response);
         self.HomeTypeData = resp.response;
       }
-
     })
+  }
+
+  setupHomeName(){
+    let self = this;
+    let params = {
+      "villageId": this.findhomebean.villageId,
+      "homeTypeCode": this.findhomebean.homeTypeId
+    };
+    this.api.post('home/home_list_by_village_hometype', params, function (resp) {
+      if (resp != null && resp.status.toUpperCase() == "SUCCESS") {
+        console.log(resp.response);
+        self.HomeNameData = resp.response;
+      }
+    })
+  }
+
+  // changeForHomename(){
+  //   this.setupHomeName();
+  // }
+
+  addHome(){
+    if(this.findhomebean.homeId == "0"){
+
+    }
+    
   }
 
 }
