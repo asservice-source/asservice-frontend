6d345e127766d1d6a22ffc64db77631a9eeca96f
FREEWILLGROUP\Sittichok_Kum <sittichok_kum@freewillsolutions.com>
2017-11-23 15:05:57 +0700

    no message

--- src/app/beans/cancer.bean.ts
+++ src/app/beans/cancer.bean.ts
@@ -8,4 +8,60 @@ export class CancerBean{
     public other: string;
     public status: string;
     public inputDate: string;
+
+    public citizenId: string = "";
+    public firstName: string = "";
+    public lastName: string = "";
+
+    public nickName: string = "";
+    public genderCode: string = "";
+    public genderName: string = "";
+    public prefixCode: string = "";
+    public prefixName: string = "";
+    public raceCode: string = "";
+    public raceName: string = "";
+    public nationalityCode: string = "";
+    public nationalityName: string = "";
+    public religionCode: string = "";
+    public religionName: string = "";
+    public bloodTypeId: string = "";
+    public bloodTypeName: string = "";
+    public rHGroupId: string = "";
+    public rHGroupName: string = "";
+    public birthDate: any;
+    public educationCode: string = "";
+    public educationName: string = "";
+    public educationDescription: string = "";
+    public occupationCode: string = "";
+    public occupationName: string = "";
+    public dischargeId: string = "";
+    public familyStatus: string = "";
+ 
+    public address : string;
+    public gender : string;
+
+    public patientSurveyTypeCode : string;
+    public diseaseStatusType : string;
+    public cancerDate : string;
+    public hInsuranceTypeID : string;
+    public disabilityCauseTypeID : string;
+
+    public disabilityTypeID : string;
+    public disabilityCauseType : string;
+    public remark : string;
+    public telephone : string;
+    public operationDate : string;
+    public homeId : string;
+
+    public cancerTypeId : string;
+    public rowGUID : string;
+    public personId : string;
+    public documentId : string;
+    public osmId : string;
+    public cancerTypeID : string;
+    public diseaseStatusTypeID : string;
+
+    public treatmentPlace : string;
+    public latitude : string;
+    public longitude : string;
 }
