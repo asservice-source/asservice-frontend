84e63830e9b79a928d1c5701f1eb10b038d0c507
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2017-12-27 13:36:23 +0700

    no message

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { RequestOptions, Headers, RequestMethod, Http ,Response} from '@angular/http';
+
 declare var $: any
 @Component({
   selector: 'app-content',
@@ -10,11 +11,31 @@ declare var $: any
 export class ContentComponent implements OnInit {
   [x: string]: any;
   headers: any;
+
+  public positions: any = [];
+  public pos = '16.442481, 102.808265';
  
   constructor(private http: Http) { }
 
   ngOnInit() {
     
+  }
+
+  onMapReady(map) {
+    console.log('map', map);
+    console.log('markers', map.markers);  // to get all markers as an array 
+  }
+  onIdle(event) {
+    console.log('map', event.target);
+  }
+  onMarkerInit(marker) {
+    this.pos = marker;
+    console.log('marker', marker);
+  }
+  onMapClick(event) {
+    this.pos = event.latLng;
+    // this.positions.push(event.latLng);
+    // event.target.panTo(event.latLng);
   }
 
 }
