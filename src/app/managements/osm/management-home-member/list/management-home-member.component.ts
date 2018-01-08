4a19776d65d2a7366d9f21c69381c1ada73be214
FREEWILLGROUP\Terdsak_Pot <terdsak_pot>
2018-01-08 17:14:42 +0700

    no message

--- src/app/managements/osm/management-home-member/list/management-home-member.component.ts
+++ src/app/managements/osm/management-home-member/list/management-home-member.component.ts
@@ -20,7 +20,9 @@ export class ManagementHomeMemberComponent extends BaseComponent implements OnIn
   public bean: PersonalBasicBean;
   public action: string;
   public settings: any;
-  public source: LocalDataSource = new LocalDataSource();
+  public deadSettings: any;
+  public source: LocalDataSource;
+  public deadSource: LocalDataSource;
   public api: Service_HomeMember;
   public homeInfo: any;
   public isShowInfo: boolean = false;
@@ -28,8 +30,11 @@ export class ManagementHomeMemberComponent extends BaseComponent implements OnIn
   public address: Address;
   public loading: boolean = false;
   public isStaff: boolean;
+  public isShowDeadList: boolean = false;
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private changeRef: ChangeDetectorRef) { 
     super();
+    this.source = new LocalDataSource();
+    this.deadSource = new LocalDataSource();
     this.bean = new PersonalBasicBean();
     this.api = new Service_HomeMember();
     this.address = new Address();
@@ -38,6 +43,114 @@ export class ManagementHomeMemberComponent extends BaseComponent implements OnIn
     }else{
       this.isStaff = false;
     }
+    this.settingColumns();
+  }
+
+  ngOnInit() {
+    let _self = this;
+    this.activatedRoute.params.subscribe(params => {
+      _self.homeId = params['homeId'];
+
+      _self.setupHomeInfo();
+      _self.setupTable();
+    });
+  }
+
+  setupHomeInfo(){
+    let _self = this;
+    _self.api.api_HomrInfo(_self.homeId, function(response){ 
+      _self.isShowInfo = true;
+      _self.homeInfo = response.response;
+      _self.address.homeNo = _self.homeInfo.homeNo;
+      _self.address.mooNo = _self.homeInfo.villageNo;
+      _self.address.road = _self.homeInfo.road;
+      _self.address.tumbolCode = _self.homeInfo.tumbolCode;
+      _self.address.amphurCode = _self.homeInfo.amphurCode;
+      _self.address.provinceCode = _self.homeInfo.provinceCode;
+    });
+    
+  }
+  setupTable(){
+    let _self = this;
+    _self.loading = true;
+    _self.api.getList(_self.homeId, true, function(response){
+      let members = [];
+      let deadMembers = [];
+      for(let item of response){
+        if(!item.isDead){
+          members.push(item);
+        }else{
+          deadMembers.push(item);
+        }
+      }
+      _self.source = _self.ng2STDatasource(members);
+      if(deadMembers.length>0){
+        _self.isShowDeadList = true;
+        _self.deadSource = _self.ng2STDatasource(deadMembers);
+      }else{
+        _self.isShowDeadList = false;
+      }
+      
+      _self.loading = false;
+      // detectChangeRef
+      $('.ng2-smart-sort').click();
+      _self.changeRef.detectChanges();
+
+    });
+  }
+  onModalShow(action){
+    this.action = action;
+    this.changeRef.detectChanges();
+    $('#modal-management-home-member-form').modal();
+  }
+  onEdit(row: any){
+    let _self = this;
+    _self.loading = true;
+    _self.api.api_PersonByPersionId(row.personId, function(resp){
+      _self.loading = false;
+      let response = resp.response;
+      if(response && resp.status.toUpperCase()=='SUCCESS'){
+        _self.bean = response;
+        _self.onModalShow(_self.ass_action.EDIT);
+      }else{
+        _self.message_servNotRespond('', '');
+      }
+      
+    });
+    
+  }
+  onClickAdd(){
+    this.bean = new PersonalBasicBean();
+    this.bean.homeId = this.homeId;
+    this.onModalShow(this.ass_action.ADD);
+  }
+
+  onCompleted(event: any){
+    console.log(event);
+    let _self = this;
+    if(event.success){
+      $('#modal-management-home-member-form').modal('hide');
+      _self.message_success('',event.message, function(){
+        _self.setupTable();
+      });
+      
+    }else{
+      _self.message_error('', event.message);
+    }
+  }
+
+  onBack(){
+    if(this.isStaff){
+      this.router.navigate(['main/managements/osm/home', 'PD', this.homeInfo.osmId]);
+    }else{
+      this.router.navigate(['main/managements/osm/home','T','01']);
+    }
+  }
+  onBackStaffManageOSM(){
+    this.router.navigate(['main/managements/staff/osm']);
+  }
+
+  settingColumns(){
     let _self = this;
     this.settings = this.getTableSetting({
       fullName: {
@@ -99,91 +212,45 @@ export class ManagementHomeMemberComponent extends BaseComponent implements OnIn
         }
       }
     });
-  }
-
-  ngOnInit() {
-    let _self = this;
-    this.activatedRoute.params.subscribe(params => {
-      _self.homeId = params['homeId'];
-
-      _self.setupHomeInfo();
-      _self.setupTable();
-    });
-  }
-
-  setupHomeInfo(){
-    let _self = this;
-    _self.api.api_HomrInfo(_self.homeId, function(response){ 
-      _self.isShowInfo = true;
-      _self.homeInfo = response.response;
-      _self.address.homeNo = _self.homeInfo.homeNo;
-      _self.address.mooNo = _self.homeInfo.villageNo;
-      _self.address.road = _self.homeInfo.road;
-      _self.address.tumbolCode = _self.homeInfo.tumbolCode;
-      _self.address.amphurCode = _self.homeInfo.amphurCode;
-      _self.address.provinceCode = _self.homeInfo.provinceCode;
-    });
-    
-  }
-  setupTable(){
-    let _self = this;
-    _self.loading = true;
-    _self.api.getList(_self.homeId, true, function(response){
-      _self.source = _self.ng2STDatasource(response);
-      _self.loading = false;
-      // detectChangeRef
-      $('.ng2-smart-sort').click();
-      _self.changeRef.detectChanges();
-
-    });
-  }
-  onModalShow(action){
-    this.action = action;
-    this.changeRef.detectChanges();
-    $('#modal-management-home-member-form').modal();
-  }
-  onEdit(row: any){
-    let _self = this;
-    _self.api.api_PersonByPersionId(row.personId, function(resp){
-      let response = resp.response;
-      if(response && resp.status.toUpperCase()=='SUCCESS'){
-        _self.bean = response;
-        _self.onModalShow(_self.ass_action.EDIT);
-      }else{
-        _self.message_servNotRespond('', '');
+    this.deadSettings = this.getTableSetting({
+      fullName: {
+        title: "ชื่อ - สกุล",
+        filter: false
+      },
+      citizenId: {
+        title: "เลขประจำตัวประชาชน",
+        filter: false,
+        width: '200px',
+        type: 'html',
+        valuePrepareFunction: (cell, row) => { 
+          return '<div class="text-center">'+_self.formatCitizenId(cell)+'</div>'
+        }
+      },
+      genderName:{
+        title: "เพศ",
+        filter: false,
+        type: 'html',
+        valuePrepareFunction: (cell, row) => { 
+          return '<div class="text-center">'+cell+'</div>'
+        }
+      },
+      // age:{
+      //   title: "อายุ",
+      //   filter: false,
+      //   type: 'html',
+      //   valuePrepareFunction: (cell, row) => { 
+      //     return '<div class="text-center">'+cell+'</div>'
+      //   }
+      // },
+      deadDate:{
+        title: "วันที่เสียชีวิต",
+        filter: false,
+        type: 'html',
+        valuePrepareFunction: (cell, row) => { 
+          return '<div class="text-center">'+_self.displayFormatDate(cell)+'</div>'
+        }
       }
-      
     });
-    
-  }
-  onClickAdd(){
-    this.bean = new PersonalBasicBean();
-    this.bean.homeId = this.homeId;
-    this.onModalShow(this.ass_action.ADD);
-  }
 
-  onCompleted(event: any){
-    console.log(event);
-    let _self = this;
-    if(event.success){
-      $('#modal-management-home-member-form').modal('hide');
-      _self.message_success('',event.message, function(){
-        _self.setupTable();
-      });
-      
-    }else{
-      _self.message_error('', event.message);
-    }
-  }
-
-  onBack(){
-    if(this.isStaff){
-      this.router.navigate(['main/managements/osm/home', 'PD', this.homeInfo.osmId]);
-    }else{
-      this.router.navigate(['main/managements/osm/home','T','01']);
-    }
-  }
-  onBackStaffManageOSM(){
-    this.router.navigate(['main/managements/staff/osm']);
   }
 }
