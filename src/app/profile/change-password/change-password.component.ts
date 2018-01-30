a73aff61496d1662908fde7e03368c0e231d58b6
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2018-01-30 15:08:45 +0700

    no message

--- src/app/profile/change-password/change-password.component.ts
+++ src/app/profile/change-password/change-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../../base-component';
 import { Router } from '@angular/router';
 import { InputValidateInfo } from '../../directives/inputvalidate.directive';
+import { Service_Profile } from '../../api-managements/service-profile';
 
 @Component({
   selector: 'app-change-password',
@@ -11,11 +12,19 @@ import { InputValidateInfo } from '../../directives/inputvalidate.directive';
 
 export class ChangePasswordComponent extends BaseComponent implements OnInit {
 
+  private apiHttp: Service_Profile = new Service_Profile();
+
   public oldPassword: string = "";
   public newPassword: string = "";
   public confirmNewPassword: string = "";
 
-  public validatePassword: InputValidateInfo = new InputValidateInfo();
+  public validateOldPassword: InputValidateInfo = new InputValidateInfo();
+  public validateNewPassword: InputValidateInfo = new InputValidateInfo();
+  public validateConfirmNewPassword: InputValidateInfo = new InputValidateInfo();
+
+  public error_message_oldPassword: string = "กรุณาระบุ รหัสผ่านเก่า";
+  public error_message_newPassword: string = "กรุณาระบุ รหัสผ่านใหม่";
+  public error_message_confirmNewPassword: string = "กรุณาระบุ ยืนยันรหัสผ่านใหม่";
 
   constructor(private route: Router) {
     super();
@@ -28,14 +37,70 @@ export class ChangePasswordComponent extends BaseComponent implements OnInit {
   onClickSave() {
     let self = this;
 
-    self.validatePassword = new InputValidateInfo();
-    self.validatePassword.isCheck = true;
+    let countError = 0;
+
+    self.validateOldPassword = new InputValidateInfo();
+    self.validateOldPassword.isCheck = true;
+
+    self.validateNewPassword = new InputValidateInfo();
+    self.validateNewPassword.isCheck = true;
+
+    self.validateConfirmNewPassword = new InputValidateInfo();
+    self.validateConfirmNewPassword.isCheck = true;
+
+    if (self.isEmpty(self.oldPassword)) {
+      self.error_message_oldPassword = "กรุณาระบุ รหัสผ่านเก่า";
+      countError++;
+    }
+
+    if (self.isEmpty(self.newPassword)) {
+      self.error_message_newPassword = "กรุณาระบุ รหัสผ่านใหม่";
+      countError++;
+    }
+
+    if (self.isEmpty(self.confirmNewPassword)) {
+      self.error_message_confirmNewPassword = "กรุณาระบุ ยืนยันรหัสผ่านใหม่";
+      countError++;
+    }
+
+    if (self.newPassword != self.confirmNewPassword) {
+      self.error_message_confirmNewPassword = "รหัสผ่านใหม่และยืนยันรหัสผ่านใหม่ต้องมีค่าเหมือนกัน";
+      self.validateConfirmNewPassword = new InputValidateInfo();
+      self.validateConfirmNewPassword.isCheck = true;
+      self.validateConfirmNewPassword.isShowError = true;
+      countError++;
+    }
+
+    if (countError > 0) {
+      return;
+    }
+
+    let userLoginId = self.userInfo.userId;
+    self.apiHttp.change_password(userLoginId, self.oldPassword, self.newPassword, function (d) {
+      if (d != null && d.status.toUpperCase() == "SUCCESS") {
+        self.message_success('', 'เปลี่ยนรหัสผ่านสำเร็จ', function () {
+          self.route.navigate(['']);
+        });
+      } else {
+        self.message_error('', 'เปลี่ยนรหัสผ่านไม่สำเร็จ', function () {
+          self.clearData();
+        });
+      }
+    });
   }
 
   onClickCancel() {
     let self = this;
 
     self.route.navigate(['']);
+  }
+
+  clearData() {
+    let self = this;
+
+    self.oldPassword = "";
+    self.newPassword = "";
+    self.confirmNewPassword = "";
   }
 
 }
