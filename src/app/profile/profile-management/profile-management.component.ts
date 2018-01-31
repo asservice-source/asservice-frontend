bb4d7e52723b8a91d6f7a7f19fab27356f18f578
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2018-01-31 17:05:25 +0700

    no message

--- src/app/profile/profile-management/profile-management.component.ts
+++ src/app/profile/profile-management/profile-management.component.ts
@@ -3,25 +3,39 @@ import { BaseComponent } from '../../base-component';
 import { Router } from '@angular/router';
 import { InputValidateInfo } from '../../directives/inputvalidate.directive';
 import { Service_Profile } from '../../api-managements/service-profile';
+import { FancyImageUploaderOptions, UploadedFile } from 'ng2-fancy-image-uploader';
 
 @Component({
-  selector: 'app-profile-management',
-  templateUrl: './profile-management.component.html',
-  styleUrls: ['./profile-management.component.css']
+    selector: 'app-profile-management',
+    templateUrl: './profile-management.component.html',
+    styleUrls: ['./profile-management.component.css']
 })
 
 export class ProfileManagementComponent extends BaseComponent implements OnInit {
+
     public validate: InputValidateInfo = new InputValidateInfo();
 
-    constructor(private route: Router){
+    options: FancyImageUploaderOptions = {
+        thumbnailHeight: 250,
+        thumbnailWidth: 250,
+        uploadUrl: 'D:/',
+        allowedImageTypes: ['image/png', 'image/jpeg'],
+        maxImageSize: 3
+    };
+
+    constructor(private route: Router) {
         super();
-        
     }
-    onClickSave(){
-        
+
+    onClickSave() {
+
     }
-    onClickCancel(){
+
+    onClickCancel() {
         this.route.navigate(['']);
     }
 
+    onUpload(file: UploadedFile) {
+        console.log(file.response);
+    }
 }
