2556b3e033c4a9ebc3acbf9478ec991086407226
Mori <panelmori@gmail.com>
2018-02-13 16:33:51 +0700

    no message

--- src/app/index/content/content.component.ts
+++ src/app/index/content/content.component.ts
@@ -15,22 +15,23 @@ export class IndexContentComponent implements OnInit{
     // nextButton: '.swiper-button-next',
     // prevButton: '.swiper-button-prev',
     // spaceBetween: 20
-
+    autoHeight: true,
     effect: 'coverflow',
     centeredSlides: true,
     slidesPerView: 'auto',
     loop: true,
+    autoplay: 3000,
     pagination: '.swiper-pagination',
     paginationClickable: true,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
-    coverflow: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows : true,
-    }
+    // coverflow: {
+    //   rotate: 50,
+    //   stretch: 0,
+    //   depth: 100,
+    //   modifier: 1,
+    //   slideShadows : true,
+    // }
   };
   constructor(public user: UserService, private router: Router){
  
