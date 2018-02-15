a278807540f288a11ad2437db4fc70229e1fb038
Mori <panelmori@gmail.com>
2018-02-15 16:48:04 +0700

    no message

--- src/app/index/about-system/about-system.component.ts
+++ src/app/index/about-system/about-system.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-index-about-system',
   templateUrl: './about-system.component.html',
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class IndexAboutSystemComponent implements OnInit{
 
     ngOnInit(): void {
-
+      setTimeout(function(){
+        let pathName = location.pathname;
+        $('.menu-item>a.active').removeClass('active');
+        $('.header-menu>ul>li.menu-item > a[href="'+pathName+'"]').addClass('active');
+      }
+      , 300);
     }
 
 }
--- src/app/index/contactus/contactus.component.ts
+++ src/app/index/contactus/contactus.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-index-contactus',
   templateUrl: './contactus.component.html',
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class IndexContactusComponent implements OnInit{
 
     ngOnInit(): void {
-
+      setTimeout(function(){
+        let pathName = location.pathname;
+        $('.menu-item>a.active').removeClass('active');
+        $('.header-menu>ul>li.menu-item > a[href="'+pathName+'"]').addClass('active');
+      }
+      , 300);
     }
 
 }
