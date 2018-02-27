7dcfe08fb1c0c19a65ca8df1b09b7ed0acf96b16
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2018-02-27 14:09:00 +0700

    no message

--- src/app/surveys/cancer/survey-cancer-history/survey-cancer-history.component.ts
+++ src/app/surveys/cancer/survey-cancer-history/survey-cancer-history.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { BaseComponent } from '../../../base-component';
 import { ActivatedRoute } from '@angular/router';
+import { CancerBean } from '../../../beans/cancer.bean';
+declare var $;
 
 @Component({
   selector: 'app-survey-cancer-history',
@@ -9,13 +11,37 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SurveyCancerHistoryComponent extends BaseComponent implements OnInit {
 
+  @Input() data: CancerBean;
+
+  public cancerBean: CancerBean;
+
   public loading: boolean = false;
 
-  constructor() {
+  constructor(private changeRef: ChangeDetectorRef) {
     super();
-   }
+
+    let self = this;
+
+    self.cancerBean = new CancerBean();
+  }
 
   ngOnInit() {
+    let self = this;
+
+    self.onModalEvent();
+  }
+
+  onModalEvent() {
+    let self = this;
+
+    $('#modal-cancer-history').on('show.bs.modal', function (e) {
+      self.cancerBean = self.data;
+      self.changeRef.detectChanges();
+    });
+
+    $('#modal-cancer-history').on('hidden.bs.modal', function () {
+
+    });
   }
 
 }
