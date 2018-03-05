38b6574b6b6d97be06a65963678114e53af66f11
Mori <panelmori@gmail.com>
2018-03-05 13:34:51 +0700

    no message

--- src/app/surveys/metabolic/survey-metabolic-list/survey-metabolic-list.component.ts
+++ src/app/surveys/metabolic/survey-metabolic-list/survey-metabolic-list.component.ts
@@ -226,11 +226,7 @@ export class SurveyMetabolicListComponent extends BaseComponent implements OnIni
         self.param_reset++;
         self.changeRef.detectChanges();
       }
-<<<<<<< HEAD
-
-=======
       $("#modalMultiMaps").modal("show");
->>>>>>> 407bc7c9ef66b2a16a49dafa7e7c3aa0c6973e7b
       self.loading = false;
     });
   }
