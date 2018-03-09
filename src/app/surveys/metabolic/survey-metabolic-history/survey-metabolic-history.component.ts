2c69968e00059e65077a06bc783c2982d186ebf2
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2018-03-09 14:47:21 +0700

    no message

--- src/app/surveys/metabolic/survey-metabolic-history/survey-metabolic-history.component.ts
+++ src/app/surveys/metabolic/survey-metabolic-history/survey-metabolic-history.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { BaseComponent } from '../../../base-component';
+import { MetabolicBean } from '../../../beans/metabolic.bean';
+declare var $;
 
 @Component({
   selector: 'app-survey-metabolic-history',
   templateUrl: './survey-metabolic-history.component.html',
   styleUrls: ['./survey-metabolic-history.component.css']
 })
-export class SurveyMetabolicHistoryComponent implements OnInit {
+export class SurveyMetabolicHistoryComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  @Input() data: MetabolicBean;
+
+  public metabolicbean: MetabolicBean;
+
+  constructor(private changeRef: ChangeDetectorRef) { 
+    super();
+
+    let self = this;
+
+    self.metabolicbean = new MetabolicBean();
+  }
 
   ngOnInit() {
+    let self = this;
+
+    self.onModalEvent();
+  }
+
+  onModalEvent() {
+    let self = this;
+
+    $('#modal-history-metabolic').on('show.bs.modal', function (e) {
+      self.metabolicbean = self.data;
+      self.changeRef.detectChanges();
+    });
+
+    $('#modal-history-metabolic').on('hidden.bs.modal', function () {
+
+    });
   }
 
 }
