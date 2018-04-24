dcdacb68529a4129859bca18774637c508fe477e
Mori <panelmori@gmail.com>
2018-04-24 16:10:47 +0700

    no message

--- src/app/global-config.ts
+++ src/app/global-config.ts
@@ -1,5 +1,5 @@
-//export const API_SERVER_URL = 'http://192.168.1.203:8080/api-asservice-front/';
-export const API_SERVER_URL = 'http://192.168.1.203:8080/api-session-demo/';
+export const API_SERVER_URL = 'http://192.168.1.203:8080/api-asservice-front/';
+//export const API_SERVER_URL = 'http://192.168.1.203:8080/api-session-demo/';
 export const API_MAPS_KEY = 'AIzaSyBpBMthbC5-MRsz8Vga99LLlxMDibt24dc';
 
 export class ReportPath{
