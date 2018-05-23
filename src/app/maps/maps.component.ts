3da1d4b8eaa1aebe926f8da4d3ba1f63fafa18e3
Pranop_Boo <Pranop_Boo@FreewillSolutions.com>
2018-05-23 17:31:17 +0700

    no message

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -31,13 +31,60 @@ export class MapsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+
   }
 
   ngOnChanges() {
     let self = this;
 
-    self.InitMap();
+    console.log('Map -> ngOnChanges -> ', self.latitude + ', ' + self.longitude);
+
+    if (!self.isEmpty(self.latitude) && !self.isEmpty(self.longitude)) {
+      let lat = +self.latitude;
+      let lng = +self.longitude;
+      let latlng = lat + ',' + lng
+
+      self.position = latlng;
+      self.info_content = self.info;
+
+      self.center = latlng;
+      // self.zoom = 15;
+      if (self.map) {
+        self.map.panTo({ lat: lat, lng: lng });
+        // self.map.setZoom(15);
+      }
+    } else {
+      self.position = "";
+
+      // เก็บค่าไว้ในตัวแปร เมื่อมีค่าแล้วจะไม่ call api อีก
+      if (self.isEmpty(self.defaultLat) && self.isEmpty(self.defaultLng)) {
+        self.getLocationGooglemaps(null, function (d) {
+          if (d.results) {
+            self.defaultLat = d.results[0].geometry.location.lat;
+            self.defaultLng = d.results[0].geometry.location.lng;
+          }
+        });
+      }
+
+      // Set center for maps
+      if (!self.isEmpty(self.defaultLat) && !self.isEmpty(self.defaultLng)) {
+        self.center = self.defaultLat + ',' + self.defaultLng;
+
+        self.zoom = 15;
+        if (self.map) {
+          self.map.panTo({ lat: self.defaultLat, lng: self.defaultLng });
+          self.map.setZoom(15);
+        }
+      } else {
+        let adr = 'ตำบล' + self.userInfo.hospitalTumbolName + ' อำเภอ' + self.userInfo.hospitalAmphurName + ' จังหวัด' + self.userInfo.hospitalProvinceName + ' ' + self.userInfo.hospitalZipCode;
+        self.center = adr;
+
+        self.zoom = 15;
+        if (self.map) {
+          self.map.setZoom(15);
+        }
+      }
+    }
   }
 
   onMapReady(map) {
@@ -125,52 +172,6 @@ export class MapsComponent extends BaseComponent implements OnInit {
       self.positionChanged.emit(objLatLng);
     } else {
 
-    }
-  }
-
-  InitMap() {
-    let self = this;
-
-    let lat = +self.latitude;
-    let lng = +self.longitude;
-
-    if (self.latitude && self.longitude) {
-      let latlng = lat + ',' + lng
-      self.center = latlng;
-      self.position = latlng;
-      self.info_content = self.info;
-      if (self.map) {
-        self.map.panTo({ lat: lat, lng: lng });
-        self.map.setZoom(15);
-      }
-      self.zoom = 15;
-    } else {
-      self.position = "";
-
-      // เก็บค่าไว้ในตัวแปร เมื่อมีค่าแล้วจะไม่ call api อีก
-      if (!self.defaultLat && !self.defaultLng) {
-        self.getLocationGooglemaps(null, function (d) {
-          if (d.results) {
-            self.defaultLat = d.results[0].geometry.location.lat;
-            self.defaultLng = d.results[0].geometry.location.lng;
-          }
-        });
-      }
-
-      // Set center for maps
-      if (self.defaultLat && self.defaultLng) {
-        self.center = self.defaultLat + ',' + self.defaultLng;
-      } else {
-        let adr = 'ตำบล' + self.userInfo.hospitalTumbolName + ' อำเภอ' + self.userInfo.hospitalAmphurName + ' จังหวัด' + self.userInfo.hospitalProvinceName + ' ' + self.userInfo.hospitalZipCode;
-        self.center = adr;
-      }
-
-      // Set zoom for maps
-      self.zoom = 15;
-      if (self.map) {
-        self.map.panTo({ lat: self.defaultLat, lng: self.defaultLng });
-        self.map.setZoom(15);
-      }
     }
   }
 
